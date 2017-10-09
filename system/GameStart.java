0dad20522e5947ce5276495a4408ad554cfc34f2
inferee <ali.yifanfengshun@gmail.com>
2017-10-09 13:56:52 -0700

    Add files via upload

--- system/GameStart.java
+++ system/GameStart.java
@@ -3,32 +3,37 @@ package system;
 import java.util.ArrayDeque;
 
 import graphics.PlayerGraphics;
+import graphics.SpectatorGraphics;
 
 public class GameStart {
 
 	public static void main(String[] args) throws InterruptedException {
-		Space[][] board = new Space[18][18];
-		for (int i = 0; i < 18; i++) {
-			for (int j = 0; j < 18; j++) {
-				if (i > 4 && i < 13 && j > 4 && j < 13) {
-					if (i < 9) {
-						board[i][j] = new Territory(1, 1);
-					} else {
-						board[i][j] = new Territory(2, 1);
-					}
-				} else {
-					board[i][j] = new Space(true);
-				}
-			}
-		}
-		board[5][9] = new City(1, 30, true);
-		board[12][8] = new City(2, 30, true);
-		ArrayDeque<Move>[] queues = new ArrayDeque[] { new ArrayDeque<Move>(), new ArrayDeque<Move>() };
-		Player[] players = new Player[] { new PlayerGraphics(18, 18, 2, 1, queues[0]),
-				new PlayerGraphics(18, 18, 2, 2, queues[1]) };
-		GameBoard g = new GameBoard(board, players, queues, new Spectator[0], 0.1, 0.1, 2, 9, 7);
-		// GameBoard g = new GameBoard(18, 18, 0.1, 0.04, 2, 15, 9);
-		// GameBoard g = new GameBoard(18, 18, 0.1, 0.04, 8, 8, 4);
+		// Space[][] board = new Space[18][18];
+		// for (int i = 0; i < 18; i++) {
+		// for (int j = 0; j < 18; j++) {
+		// if (i > 4 && i < 13 && j > 4 && j < 13) {
+		// if (i < 9) {
+		// board[i][j] = new Territory(1, 1);
+		// } else {
+		// board[i][j] = new Territory(2, 1);
+		// }
+		// } else {
+		// board[i][j] = new Space(true);
+		// }
+		// }
+		// }
+		// board[5][9] = new City(1, 30, true);
+		// board[12][8] = new City(2, 30, true);
+		// ArrayDeque<Move>[] queues = new ArrayDeque[] { new ArrayDeque<Move>(), new
+		// ArrayDeque<Move>() };
+		// Player[] players = new Player[] { new PlayerGraphics(18, 18, 2, 1,
+		// queues[0]),
+		// new PlayerGraphics(18, 18, 2, 2, queues[1]) };
+		// GameBoard g = new GameBoard(board, players, queues, new Spectator[] { new
+		// SpectatorGraphics(18, 18, 2) }, 0.1,
+		// 0.1, 2, 9, 7);
+		// GameBoard g = new GameBoard(18, 18, 0.1, 0.04, 2, 18, 9);
+		GameBoard g = new GameBoard(18, 18, 0.1, 0.04, 8, 8, 6);
 		long before = System.nanoTime();
 		long time = 0;
 		long tick = 0;
@@ -61,14 +66,21 @@ public class GameStart {
 	 * GameBoard constructor, for reference, is (i,j,proportion mountains,
 	 * proportion cities, kingdoms, min maze distance, min manhattan distance)
 	 * 
-	 * For Space[][] to Viewed[][]: ..............................................
-	 * Mountain - (known,true,0,0,false,false) ...................................
-	 * Territory - (known,false,type,troops,false,false) .........................
-	 * Known City - (true,false,type,troops,true,false) ..........................
-	 * Unknown City - (false,true,0,0,false,false) ...............................
-	 * King - (true,false,type,troops,false,true) ................................
-	 * Unknown King - (false,true,0,0,false,false)................................
+	 * For Space[][] to Viewed[][]: ................................................
+	 * Mountain - (known,true,0,0,false,false) .....................................
+	 * Territory - (known,false,type,troops,false,false) ...........................
+	 * Known City - (true,false,type,troops,true,false) ............................
+	 * Unknown City - (false,true,0,0,false,false) .................................
+	 * King - (true,false,type,troops,false,true) ..................................
+	 * Unknown King - (false,true,0,0,false,false) .................................
 	 * 
+	 * Possible Fitness: ...........................................................
+	 * Game Ranking (1st place, 2nd place, so on) ..................................
+	 * Territory and Army ..........................................................
+	 * Number of Explored Mountains ................................................
+	 * Number of Explored Spaces ...................................................
+	 * Number of Cities/Kings Captured .............................................
+	 * Time Alive ..................................................................
 	 */
 
 	/*
